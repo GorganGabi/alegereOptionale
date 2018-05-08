f6acd5cec2f3b0ca2e9cbb417890598e97b6667f
Adrian <adrian01@protonmail.com>
2018-05-08 14:53:21 +0300

    pls work

--- Back-end/OptDist/src/OptDist/OptDist.java
+++ Back-end/OptDist/src/OptDist/OptDist.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package class_implementation;
+package OptDist;
 
 /**
  *
  * @author gabih
  */
-public class Class_implementation {
+public class OptDist {
 
     /**
      * @param args the command line arguments
