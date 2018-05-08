ef464b5f2905db7d580bec1bfda1e113ca6dd4c3
DESKTOP-5GAN8AK\gabih <gabihartobanu@email.com>
2018-05-08 15:45:44 +0300

    aaa

--- Back-end/OptDist/src/OptDist/DistributionAlgorithm.java
+++ Back-end/OptDist/src/OptDist/DistributionAlgorithm.java
@@ -1,6 +1,12 @@
 
 package OptDist;
 
+import java.util.List;
+import java.util.Map;
+
 public class DistributionAlgorithm {
-    
+    Map<Student,List<Optional>> match(StudentAdministration students)
+    {
+        
+    }
 }
---
+++ Back-end/OptDist/src/OptDist/Mamp.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OptDist;
+
+/**
+ *
+ * @author gabih
+ */
+class Mamp<T0, T1> {
+    
+}
