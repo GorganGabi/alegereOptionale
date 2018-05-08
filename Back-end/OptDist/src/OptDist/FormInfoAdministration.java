dcf7bd971b3ced96605d14274b9303b8539fc5dd
Ungureanu-Ioana <ioana000000@yahoo.com>
2018-05-08 23:14:31 +0300

    Add files via upload

---
+++ Back-end/OptDist/src/OptDist/FormInfoAdministration.java
@@ -0,0 +1,30 @@
+package OptDist;
+
+import java.util.*;
+
+public class FormInfoAdministration {
+	List<FormInfo> forms;
+	
+	void generateForms(StudentAdministration students, PackageAdministration packages) {
+		
+		for( Student i : students.studList ) {
+			FormInfo form = new FormInfo();
+			
+			for( Package j : packages.packages ) {
+				if( i.getYear() + 1 == j.getYear() )
+					form.packages.add(j);
+			}
+			
+			forms.add(form);
+		}
+			
+	}
+
+	public List<FormInfo> getForms() {
+		return forms;
+	}
+
+	public void setForms(List<FormInfo> forms) {
+		this.forms = forms;
+	}
+}
