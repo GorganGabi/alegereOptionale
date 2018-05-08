af0bfbdf8f214e2058c1588c1b7b1625aafaa68e
DESKTOP-5GAN8AK\gabih <gabihartobanu@email.com>
2018-05-08 20:55:02 +0300

    no message

--- Back-end/OptDist/src/OptDist/ExcelDump.java
+++ Back-end/OptDist/src/OptDist/ExcelDump.java
@@ -7,7 +7,7 @@ public class ExcelDump {
     Map<String,Map<String,String>> data;
     String get(String x,String y)
     {
-        
+        return null;
     }
     void ExcelDump(Map<String,Map<String,String>> newData)
     {
--- Back-end/OptDist/src/OptDist/ExcelParser.java
+++ Back-end/OptDist/src/OptDist/ExcelParser.java
@@ -4,6 +4,6 @@ package OptDist;
 public class ExcelParser {
     ExcelDump parse(String Path)
     {
-        
+        return null;
     }
 }
