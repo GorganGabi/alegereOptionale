ff9b555d962b19981bb2ebe4e8e4c02564c0bd3d
VitelSilviuConstantin <vitelsilviuconstantin@gmail.com>
2018-05-18 21:54:32 +0300

    + added documentation for ExcelDump and ExcelParser classes

--- Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/ExcelDump.java
+++ Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/ExcelDump.java
@@ -2,18 +2,37 @@ package ro.uaic.info.optdist.internal;
 
 import org.xwiki.component.annotation.Component;
 import ro.uaic.info.optdist.ExcelDumpInterface;
-
+/**
+ * This class holds and provides access to the data extracted from an xls/xlsx file.
+ * @author svitel
+ */
 @Component
 public class ExcelDump implements ExcelDumpInterface{
     
+    /**
+     * The data member is a bidimensional array of strings which will hold the information from the xls/xlsx's cells.
+     */
     private String[][] data;
+    /**
+     * nrOfRows and nrOfColumns represent the dimensions of the bidimensional array
+     */
     private int nrOfRows, nrOfColumns;
-    
+    /**
+     * This method gives acces to a cell of the bidimensional array
+     * @param x the line of the String to be retrieved
+     * @param y the column of the String to be retrieved
+     * @return the desired String
+     */
     public String get(int x, int y)
     {
         return data[x][y];
     }
-    
+    /**
+     * A simple contructor for the class. It initializes the dimensions of the bidimensional array and the array itself.
+     * @param nrOfRows
+     * @param nrOfColumns
+     * @param data 
+     */
     public ExcelDump(int nrOfRows, int nrOfColumns, String[][] data)
     {
         this.nrOfRows = nrOfRows;
--- Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/ExcelParser.java
+++ Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/ExcelParser.java
@@ -12,9 +12,25 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.xwiki.component.annotation.Component;
 import ro.uaic.info.optdist.ExcelParserInterface;
 
+/**
+ * This class has the methods required to extract information from and xls/xlsx file.
+ * @author svitel
+ */
+
 @Component
 public class ExcelParser implements ExcelParserInterface{
     
+    /**
+     * This method handles the actual parsing of the xls/xls document.
+     * It takes a path (a String type value) and returns an object of type ExcelDump (see {@link ExcelDump} section).
+     * The cells extracted from the xls/xlsx file are placed into a bidimensional array of Strings (a cell in the xls/xlsx = an entry into the bidimensional array).
+     * The bidimensional array is indexed from 0.
+     * IMPORTANT. The xls/xlsx document to be parsed needs to have the same number of completed cells on every row and column. 
+     * For example, you CAN'T have 3 completed cells on ROW 1 and 2 completed cells on ROW 2.
+     * If the xls/xlsx can have such cases please complete the remaining blank cells with a default value (Ex: N/A).  
+     * @param Path the path to the xls/xlsx document to be parsed
+     * @return an ExcelDump object
+     */
     public ExcelDump parse(String Path)
     {   
         File fileHandle;
