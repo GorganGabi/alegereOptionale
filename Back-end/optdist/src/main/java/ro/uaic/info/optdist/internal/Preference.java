2988cd770491a1146ebe98ba417f5b34d58a907e
rbaisan <rbaisan@bitdefender.com>
2018-05-18 23:08:47 +0300

    +Preference docs

--- Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/Preference.java
+++ Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/Preference.java
@@ -8,10 +8,19 @@ import org.xwiki.component.annotation.Component;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class is used to represent the preference of one student,
+ * as a map between each package and an ordered list of optionals.
+ * The map is the variable <code>preference</code>.
+ */
 @Component
 public class Preference implements PreferenceInterface {
     private  Map<Package, List<Optional>> preference;
 
+    /**
+     * Constructor.
+     * @param studentInput the input from a student
+     */
     public Preference(FormResponse studentInput)
     {
         for (Map.Entry<Package, List<Optional>> entry : studentInput.getPrefs().entrySet()){
@@ -20,14 +29,14 @@ public class Preference implements PreferenceInterface {
     }
 
     /**
-     * @return the preference
+     * @return the preference map
      */
     public Map<Package, List<Optional>> getPreference() {
         return preference;
     }
 
     /**
-     * @param preference the preference to set
+     * @param preference the preference map to set
      */
     public void setPreference(Map<Package, List<Optional>> preference) {
         this.preference = preference;
