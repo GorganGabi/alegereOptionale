f8638371c1bdfc719873a0c13cdf4cb75b98a781
Ungureanu-Ioana <ioana000000@yahoo.com>
2018-05-18 23:04:59 +0300

    + added doc

--- Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/Package.java
+++ Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/Package.java
@@ -6,6 +6,12 @@ import org.xwiki.component.annotation.Component;
 
 import java.util.List;
 
+/**
+ * Contains a static atribute, packageCount, that counts the packages as we add them
+ * and all the atributes that describe a package: a list of optionals, 
+ * a year, a semester, a name and an ID.
+ *
+ */
 @Component
 public class Package implements PackageInterface{
     private List<Optional> optionals;
@@ -15,6 +21,13 @@ public class Package implements PackageInterface{
     private String ID;
     private static int packageCount = 0;
     
+	/** Constructor that besides assigning also raises the packageCount.
+     * 
+     * @param optionals this package's list of optionals
+     * @param year the package's year
+     * @param semester the package's semester 
+     * @param ID the package's ID
+     */
     public Package(List<Optional> optionals, int year, int semester, String ID) {
         this.optionals = optionals;
         this.year = year;
@@ -29,62 +42,88 @@ public class Package implements PackageInterface{
         this.ID = packageCount;
     }
 */
-    
+    /** The function returns this package's list of optionals.
+     * 
+     * @see Optional
+     * @return A list of Optionals
+     */
     public List<Optional> getOptionals()
     {
         return optionals;
     }
 
+     /** The function returns this package's year.
+     * 
+     * @return package's year
+     */
     public int getYear()
     {
         return year;
     }
 
+    /** The function returns this package's semester.
+    * 
+    * @return package's semester
+    */
     public int getSemester()
     {
         return semester;
     }
-
+	
+    /** The function returns this package's name.
+     * 
+     * @return package's name
+     */
     public String getName()
     {
         return name;
     }
     
+    /** The function returns this package's ID.
+     * 
+     * @return package's ID
+     */
     public String getID()
     {
         return ID;
     }
-
-    /**
-     * @param optionals the optionals to set
+	
+    /**Sets the package's optional list.
+     * 
+     * @param package's new optional list
+     * @see Optional
      */
     public void setOptionals(List<Optional> optionals) {
         this.optionals = optionals;
     }
 
-    /**
-     * @param year the year to set
+    /**Sets the package's year.
+     * 
+     * @param package's new oyear
      */
     public void setYear(int year) {
         this.year = year;
     }
 
-    /**
-     * @param semester the semester to set
+    /**Sets the package's semester.
+     * 
+     * @param package's new semester
      */
     public void setSemester(int semester) {
         this.semester = semester;
     }
 
-    /**
-     * @param name the name to set
+    /**Sets the package's name.
+     * 
+     * @param package's new name
      */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @param ID the ID to set
+    /**Sets the package's ID.
+     * 
+     * @param package's new ID
      */
     public void setID(String ID) {
         this.ID = ID;
