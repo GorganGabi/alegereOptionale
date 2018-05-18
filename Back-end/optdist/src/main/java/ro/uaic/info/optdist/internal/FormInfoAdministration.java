1a1111ec3a5949de9fb09a2f60e3739c309f5631
Ungureanu-Ioana <ioana000000@yahoo.com>
2018-05-18 22:04:33 +0300

    +FormInfoAdministration docs

--- Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/FormInfoAdministration.java
+++ Back-end/optdist/src/main/java/ro/uaic/info/optdist/internal/FormInfoAdministration.java
@@ -10,6 +10,22 @@ import java.util.*;
 public class FormInfoAdministration implements FormInfoAdministrationInterface {
 	private List<FormInfo> forms;
 	
+	/**The function parses the list of students that is offered through the first parameter, 
+	 * it creates a new form for each student then parses the package list we receive through
+	 * the second parameter. If the year in which the student is about to pass into is equal to 
+	 * the year of a package then that package is added to the form of that student.
+	 * 
+	 * At the end, the form created specifically for the Student is added to the list of forms
+	 * this class contains.
+	 * 
+	 * @see StudenttAdministration
+	 * @see PackageAdministration
+	 * @see Student
+	 * @see FormInfo
+	 * @see Package
+	 * @param students - the students that we have to generate forms for
+	 * @param packages - the packages that we have to distribute to the students
+	 */
         @Override
 	public void generateForms(StudentAdministration students, PackageAdministration packages) {
 		
@@ -26,10 +42,20 @@ public class FormInfoAdministration implements FormInfoAdministrationInterface {
 			
 	}
 
+    	/** The function returns a list of all forms.
+     	* 
+     	* @see FormInfo
+     	* @return A list of forms
+     	*/
 	public List<FormInfo> getForms() {
 		return forms;
 	}
 
+	/**Sets the list of forms from this class in the one we receive as parameter.
+	 * 
+	 * @see FormInfo
+	 * @param forms - A list of forms.
+	 */
 	public void setForms(List<FormInfo> forms) {
 		this.forms = forms;
 	}
