aef9bb29c5830b38a60157fdced16be609b81995
teodoragrosu <teogrosu29@gmail.com>
2018-05-31 00:36:25 +0300

    updated test
    updated test

--- Testare/PreferenceTest.java
+++ Testare/PreferenceTest.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 /**
  *
  * @author Madalina
+ * @update: Teodora
  */
 public class PreferenceTest {
     
